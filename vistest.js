71259ed9141053901731a7f25095baea681d7e3c
Your Name <1569464191@qq.com>
2024-10-10 17:52:39 -0700

    Update vistest.js

--- vistest.js
+++ vistest.js
@@ -1,49 +1,83 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-const dataset = [
-  { platform: "PS2", count: 400 },
-  { platform: "DS", count: 2152 },
-  { platform: "PS3", count: 1331 },
-  { platform: "Wii", count: 1320 },
-  { platform: "X360", count: 1262 },
-  { platform: "PSP", count: 1209 },
-  { platform: "PC", count: 4500 },
-  { platform: "XB", count: 150 },
-  { platform: "GBA", count: 120 },
-];
 
+async function drawVis(){
+  
+
+const dataset = await d3.csv("./datasets/videogames_wide.csv", d3.autoType);
+
+console.log(dataset);
 const width = 640;
 const height = 400;
 
-const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+const margin = { top: 40, right: 40, bottom: 40, left: 40 };
 
 const svg = d3
   .select("#visContainer")
   .append("svg")
   .attr("width", width)
-  .attr("height", height);
+  .attr("height", height)
+  .style("border", "1px solid black");
 
-const g = svg
-  .append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-const xScale = d3.scaleLinear().domain([120, 4500]).range([0, 640]);
-const yScale = d3.scaleLinear().domain([120, 4500]).range([0, 400]);
 
-console.log(xScale(2190));
+const maxGlobalSales = d3.max(dataset, (d) => d.Global_Sales);
 
+const maxJpSales = d3.max(dataset, (d)=>d.JP_Sales);
+
+// 定义比例尺，适用于 `global_sales` 的值
+const xScale = d3
+  .scaleLinear()
+  .domain([0, maxGlobalSales])
+  .range([margin.left, width - margin.right]);
+
+const yScale = d3
+  .scaleLinear()
+  .domain([0,maxJpSales])
+  .range([ height - margin.bottom, margin.top ]);
+
+const colorScale = d3
+  .scaleLinear()
+  .domain([0,maxGlobalSales])
+  .range(["blue","red"]);
+
+// 在可视化中添加圆形
 svg
-  .selectAll("rect")
+  .selectAll("circle")
   .data(dataset)
-  .join("rect")
-  .attr("x", (d) => {
-    return xScale(d.count) - 20;
-  })
-  .attr("y", 50)
-  .attr("width", 10)
-  .attr("height", 10)
-  .attr("fill", () => {
-    return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
-      Math.random() * 255
-    )}, ${Math.floor(Math.random() * 255)})`;
+  .join("circle")
+  .attr("cx",
+     (d) => {return xScale(d.Global_Sales);
+     })
+     .attr("cy",
+      (d) => {return yScale(d.JP_Sales);
+      })
+  .attr("r", 2)
+  .attr("fill", (d) => {
+    try{
+      if(d.Name.toLowerCase().includes("mario")){
+      return "red";
+    }else{
+      return "grey";
+    }
+    
+  }catch(console.error();
+  )
+
   });
+
+  // 添加 x 轴
+  svg
+    .append("g")
+    .attr("transform", `translate(0, ${height - margin.bottom})`) // 确保 x 轴在底部边界
+    .call(d3.axisBottom(xScale));
+
+  // 添加 y 轴
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, 0)`) // 确保 y 轴在左侧边界
+    .call(d3.axisLeft(yScale));
+}
+
+drawVis();
+
